355bbe2fd60b731b59ab815a5f2da8d0303948a6
Danil <nerdylivefeed@gmail.com>
2018-01-23 23:46:35 +0300

    Release 1.1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+	setlocale(LC_ALL, "rus");
+cout << "\tДомик на C++ :D \n";
+cout << "\t                          \n";
+cout << "\t                          \n";
+cout << "\t        /\\               \n";
+cout << "\t       /  \\              \n";
+cout << "\t      /    \\             \n";
+cout << "\t     /      \\            \n";
+cout << "\t    /________\\           \n";
+cout << "\t    |         |           \n";
+cout << "\t    |         |           \n";
+cout << "\t    |         |           \n";
+cout << "\t    |         |           \n";
+cout << "\t    |         |           \n";
+cout << "\t    |_________|           \n";
+
+return 0;
+}
